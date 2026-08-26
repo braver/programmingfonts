4cdb87e9ca22a5217df9a2f632ee99d31f6fbb58
Koen Lageveen <braver@users.noreply.github.com>
2026-08-26 14:21:03 +0200

    renderSelectList doesn't take args

--- index.js
+++ index.js
@@ -169,6 +169,6 @@ window.addEventListener('DOMContentLoaded', () => {
       })
 
       renderSelectList()
-      new Filters(data, () => {renderSelectList(true)}).init()
+      new Filters(data, () => {renderSelectList()}).init()
     })
 })
